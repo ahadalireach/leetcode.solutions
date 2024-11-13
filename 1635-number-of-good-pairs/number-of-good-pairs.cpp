a7fecc49433ed6a1834e9ca221dc81bacf1cf5ff
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-11-13 08:10:12 +0500

    Time: 3 ms (5.68%) | Memory: 9.1 MB (37.11%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> count;
+        int res = 0;
+
+        for(int num : nums){
+            if(count.find(num) != count.end()){
+                res += count[num];
+            }
+            count[num]++;
+        }
+        
+        return res;
+        // Time complexity: O(n)
+        // Space complexity: O(n)
+    }
+};
+
+// Approach 1: 
+// Time complexity: O(n²)
+// for(int i = 0; i < n; i++)
+//     for(int j = i; j < n; j++)
+//         if(nums[i] == nums[j] && i < j) 
+//             res++;
+
+// Approach 2:
+// Time complexity: O(n²) 
+// for(int i = 0; i < n; i++)
+//     for(int j = i + 1; j < n; j++)
+//         if(nums[i] == nums[j]) 
+//             res++;

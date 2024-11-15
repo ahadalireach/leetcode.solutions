6c2bcf8d9d66703eeb55f351a39830321bdbc316
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-11-15 08:50:22 +0500

    Time: 0 ms (100.00%) | Memory: 9.4 MB (20.25%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -20,3 +20,21 @@ public:
         return q; // Time Complexity: O(n)
     }
 };
+
+// First element in case of even number
+// ListNode* middleNode(ListNode* head) {
+
+//     ListNode* p = head;
+//     ListNode* q = head;
+//     ListNode* prev_q = nullptr;
+
+//     while(p && p->next){
+//         p = p->next->next;
+//         prev_q = q; 
+//         q = q->next;
+//     }
+
+//     // If p is null, q is pointing to the first middle node in even case
+//     // If p is not null, q is pointing to the second middle node in odd case
+//     return (p == nullptr) ? prev_q : q;
+// }

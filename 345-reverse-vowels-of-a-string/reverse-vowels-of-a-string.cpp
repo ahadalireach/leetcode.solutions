c7e6b3a567cab5c371a757897577e7f478ec9ec9
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-11-15 08:23:50 +0500

    Time: 1 ms (55.89%) | Memory: 9.2 MB (93.08%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -7,42 +7,30 @@ public:
     }
 
     string reverseVowels(string s) {
-        int i = 0;
-        int j = s.size() - 1;
+        int i = 0, j = s.size() - 1;
 
-        while (i < j) {
-            if(isVowel(s[i]) && isVowel(s[j])){
-                char temp = s[i];
-                s[i] = s[j];
-                s[j] = temp;
+        while(i < j){
+            while(i < j && !isVowel(s[i]))
                 i++;
+            while(i < j && !isVowel(s[j]))
                 j--;
-            }else if(isVowel(s[i]))
-                j--;
-            else
-                i++;
-        }
-        // int i = 0, j = s.size() - 1;
-
-        // while(i < j){
-        //     while(i < j && !isVowel(s[i]))
-        //         i++;
-        //     while(i < j && !isVowel(s[j]))
-        //         j--;
             
-        //     if (i < j) {
-        //         std::swap(s[i], s[j]);
-        //         i++;
-        //         j--;
-        //     }
-        // }
-        return s;
+            if (i < j) {
+                std::swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s; // Time Complexity: O(n)
     }
 };
 
-// for(int i = 0, j = s.size() - 1; i < s.size() / 2;){
+// int i = 0;
+// int j = s.size() - 1;
+
+// while (i < j) {
 //     if(isVowel(s[i]) && isVowel(s[j])){
-//         int temp = s[i];
+//         char temp = s[i];
 //         s[i] = s[j];
 //         s[j] = temp;
 //         i++;

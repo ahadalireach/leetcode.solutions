e82823031d83f6446d5782a5b092a00096edc2d8
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-11-16 09:08:32 +0500

    Time: 8 ms (67.92%) | Memory: 10.8 MB (85.41%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,28 +9,34 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // ListNode* fast = head;
-        // ListNode* slow = head;
-        // while(fast && fast->next){
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        //     if(fast == slow)
-        //         return true;
-        // }
-        // return false;
-        std::unordered_set<ListNode*> visited;
-
-        ListNode* current = head;
-        while(current){
-            if(visited.count(current))
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow)
                 return true;
-            visited.insert(current);
-            current = current->next;
         }
         return false;
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
     }
 };
 
+// Detect cycles in O(1) space using two pointers.
+// Move one pointer by one and the other by two to find if they meet, indicating a cycle.
 
-// Move to new node and store to some data structure if elements come again then we should stop. But this will take O(n)
-// We can solve this in O(1) using two pointer approaach move one pointer by one and other by two
+
+// Move through the linked list, storing each node in a set to detect cycles.
+// This method uses extra space and has a time complexity of O(n).
+
+// std::unordered_set<ListNode*> visited;
+
+// ListNode* current = head;
+// while (current) {
+//     if (visited.count(current))
+//         return true;
+//     visited.insert(current);
+//     current = current->next;
+// }
+// return false;

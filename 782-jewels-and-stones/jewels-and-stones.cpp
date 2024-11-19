49565fbee7f34d45157d4ecc9cdbca84f7bd79a2
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-11-19 08:30:02 +0500

    Time: 2 ms (19.36%) | Memory: 8.3 MB (46.16%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -2,9 +2,20 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int count = 0;
+        unordered_map<char, char> mp;
         for(int i = 0; i < jewels.size(); i++)
-            for(int j = 0; j < stones.size(); j++)
-                if(jewels[i] == stones[j]) count++;
+            mp[jewels[i]] = jewels[i];
+        
+        for(int i = 0; i < stones.size(); i++)
+            if(mp.find(stones[i]) != mp.end())
+                count++;
         return count;
     }
 };
+
+// Time complexity: O(n²)
+// int count = 0;
+// for(int i = 0; i < jewels.size(); i++)
+//     for(int j = 0; j < stones.size(); j++)
+//         if(jewels[i] == stones[j]) count++;
+// return count;
